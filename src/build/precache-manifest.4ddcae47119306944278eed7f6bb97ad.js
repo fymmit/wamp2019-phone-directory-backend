cf4ab8638a1f2da20954a772a173007acd8da3fb
fymmit <blomqvist.timo@gmail.com>
2019-03-11 15:13:10 +0200

    Add built frontend

---
+++ src/build/precache-manifest.4ddcae47119306944278eed7f6bb97ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "abde2efcfff91acfaaef",
+    "url": "/static/js/main.1c4107fb.chunk.js"
+  },
+  {
+    "revision": "c2d7ce51657b25f6d907",
+    "url": "/static/js/2.b09e119a.chunk.js"
+  },
+  {
+    "revision": "ef90ce2c35e28d2feeb0ed1b427a95d2",
+    "url": "/index.html"
+  }
+];
